1fa5900cc2087a33d377cf37f5c72e9bded78edf
yintianxiong <yintx_129@163.com>
2021-11-16 00:37:43 +0800

    单例

--- package/Factory/Factory.php
+++ package/Factory/Factory.php
@@ -11,28 +11,50 @@ use ReflectionClass;
 
 class ProductFactory implements Factory
 {
+    protected $instances = [];
+
+    /**
+     * 单例
+     */
+    public function instance($abstract, $instance)
+    {
+        $this->instances[$abstract] = $instance;
+    }
+
     public function make($class)
     {
+        if(isset($this->instances[$class])) {
+            return $this->instances[$class];
+        }
+
         // 直接 new 类名没有办法传参
         // 引入反射
 //        return new $class;
 
-    $reflector = new ReflectionClass($class);
-//    var_dump($reflector);
-    // 获取到了构造函数
-    $constructor = $reflector->getConstructor();
-//    var_dump($constructor);die;
+        // 获取 $class 的 reflectionClass （反射）对象
+        $reflector = new ReflectionClass($class);
+//        var_dump($reflector);
+        // 获取 $class 构造函数
+        $constructor = $reflector->getConstructor();
+//        var_dump($constructor);die;
 
-    if (is_null($constructor)) {
-        return new $class;
-    }
+        if (is_null($constructor)) {
+            return new $class;
+        }
 
-    $dependencies = $constructor->getParameters();
-//    var_dump($dependencies);
+        // 获取 $class 的构造函数所有的依赖参数
+        $dependencies = $constructor->getParameters();
+//        var_dump($dependencies);
 
-    $instance = $this->resolveDependencies($dependencies);
-//    var_dump($instance);die;
-    return $reflector->newInstanceArgs($instance);
+        // 解析依赖参数
+        $instance = $this->resolveDependencies($dependencies);
+//        var_dump($instance);die;
+
+        /**
+         * 生成最后的 $class
+         * 创建一个类的新实例，给出的参数将传递到类的构造函数
+         */
+        return $reflector->newInstanceArgs($instance);
     }
 
     // 这个方法外部用不到，直接定义受保护的
--- shop.php
+++ shop.php
@@ -6,6 +6,8 @@ require __DIR__ . "/vendor/autoload.php";
 
 // 实例化工厂类
 $factory = new Factory\ProductFactory();
+$factory->instance('shoes', $factory->make('Product\Shoes'));
+$factory->instance('clothes', $factory->make('Product\Clothes'));
 
 // new 太多了，引入工厂模式
 $shopClothes = new \Shop\ShopClothes();
@@ -14,8 +16,8 @@ $shopShes = new \Shop\ShopShoes();
 //$factory->make('Product\Clothes');
 
 // 工厂模式，创造一个工厂类，用来实例化对象，再把实例化好的对象传参到另一个类中
-$shopClothes->setProduct($factory->make('Product\Clothes'));
-$shopShes->setProduct($factory->make('Product\Shoes'));
+$shopClothes->setProduct($factory->make('clothes'));
+$shopShes->setProduct($factory->make('shoes'));
 
 
 var_dump($shopClothes, $shopShes);
