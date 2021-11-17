c2ec6592661dfb68981665aa3a895d4e11922bfe
yintianxiong <yintx_129@163.com>
2021-11-18 00:18:31 +0800

    简单容器

--- TimidHaunter/laravel/framework/src/Illuminate/Container/Container.php
+++ TimidHaunter/laravel/framework/src/Illuminate/Container/Container.php
@@ -2,10 +2,75 @@
 
 namespace Illuminate\Container;
 
+use ReflectionClass;
+
 /**
  *
  */
 class Container
 {
-    
+    /**
+     * A 依赖 C，容器获取 C 的实例，注入 A
+     * 容器用来生成实例的
+     */
+    public function make($abstract)
+    {
+        return $this->resolve($abstract);
+    }
+
+    /**
+     * 解析抽象
+     * @param $abstract
+     */
+    protected function resolve($abstract)
+    {
+        $concrete = $abstract;
+
+        // 创建实例
+        return $this->build($concrete);
+    }
+
+    /**
+     * 构建实例
+     * @param $concrete
+     */
+    protected function build($concrete)
+    {
+        // 通过反射，获得反射对象
+        $reflector = new ReflectionClass($concrete);
+
+        // 获取已反射的类的构造函数，没有构造函数返回 NULL
+        $constructor = $reflector->getConstructor();
+
+        // 如果没有构造函数，就创造一个实例
+        if (is_null($constructor)){
+            return new $concrete;
+        }
+
+        // 获得依赖
+        // abstract class ReflectionFunctionAbstract implements Reflector
+        // ReflectionFunctionAbstract 中的方法
+        $dependencies = $constructor->getParameters();
+
+        // 解析依赖
+        $instances = $this->resolveDependencies($dependencies);
+
+        // 从给出的参数创建一个新的类实例（依赖）
+        return $reflector->newInstanceArgs($instances);
+    }
+
+    /**
+     * 解析依赖，什么是依赖
+     * @param $dependencies
+     */
+    protected function resolveDependencies ($dependencies)
+    {
+        $results = [];
+        foreach ($dependencies as $dependency) {
+            $results[] = $this->make($dependency->getName());
+        }
+        return $results;
+    }
+
+
 }
--- public/index.php
+++ public/index.php
@@ -2,3 +2,51 @@
 
 //var_dump(__DIR__);
 require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Class Log
+ * Log 依赖 File
+ */
+class Log
+{
+    protected $file;
+    public function __construct(File $file)
+    {
+        $this->file = $file;
+    }
+}
+
+/**
+ * Class File
+ * File 依赖 Sys
+ */
+class File
+{
+    protected $sys;
+    public function __construct(Sys $sys)
+    {
+        $this->sys = $sys;
+    }
+}
+
+class Sys
+{
+
+}
+
+$container = new \Illuminate\Container\Container();
+// 类名::class，带命名空间的类名
+
+// Log 注入容器，解析 Log 的依赖
+// 假如 File 还有依赖 Sys
+$obj = $container->make(Log::class);
+
+var_dump($obj);
+//    object(Log)#8 (1) {
+//    ["file":protected]=>
+//      object(File)#9 (1) {
+//      ["sys":protected]=>
+//        object(Sys)#10 (0) {
+//        }
+//      }
+//    }
